ceb9c8494d71b94cdf792e197481cad2b1684116
aberezn <84450664+aberezn@users.noreply.github.com>
2022-02-18 15:29:08 +0300

    Add files via upload

---
+++ 13.Generics в коллекциях и методах/Generic и наследования/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Integer i = 100;
+        Number n = i;
+
+        Memory<Number> memory = new Memory<>();
+        memory.save(3.5);
+//        memory.save("Petya");
+//        memory.save("Olya");
+//        memory.save("Tanya");
+//        String value = memory.getLast();
+
+        String v1 = "Petya";
+        String v2 = "Olya";
+        String result = choose(true, v1, v2);
+    }
+
+    public static <T> T choose(boolean flag, T first, T second) {
+        if (flag) {
+            return first;
+        } else {
+            return second;
+        }
+    }
+}
---
+++ 6.Структура класса/Поля класса/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Singer singer = new Singer();
+        singer.name = "Petya";
+        singer.age = 8;
+        singer.rating = 3;
+    }
+}
